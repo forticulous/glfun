f9a344db21aea99835d604c8d5a7c080808b7cb9
fort <e@mail.com>
2014-01-23 23:32:20 -0800

    Basic triangle, but not working yet

--- glfun.c
+++ glfun.c
@@ -2,10 +2,36 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
-static void render(void)
+const float vertices[] = {
+     0.75f,  0.75f, 0.0f,
+     0.75f, -0.75f, 0.0f,
+    -0.75f, -0.75f, 0.0f,
+};
+
+GLuint vertexBuffer;
+
+void initVertexBuffer()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glGenBuffers(1, &vertexBuffer);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void render()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
 
@@ -18,6 +44,9 @@ int main(int argcp, char** argv)
     glutDisplayFunc(&render);
 
     glewInit();
+
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+    initVertexBuffer();
 
     glutMainLoop();
     return 0;
