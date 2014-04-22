0998862ed3f586d3ec4e6ea6333e9cf08a15a14c
fort <e@mail.com>
2014-04-21 23:22:38 -0700

    Removing unnecessary kruft from vertes shader

--- shader/vs.glsl
+++ shader/vs.glsl
@@ -7,34 +7,10 @@ uniform mat4 mInvTrans;
 in vec3 position;
 in vec3 normal;
 in vec2 uv;
+
 out vec3 fposition;
 out vec3 fnormal;
 out vec2 fuv;
-
-struct lightSource {
-    vec3 position;
-    vec3 diffuse;
-    vec3 specular;
-};
-lightSource light = lightSource(
-    vec3(-8.0, 0.0, 0.0),
-    vec3(1.0, 1.0, 1.0),
-    vec3(1.0, 1.0, 1.0)
-);
-
-struct materialSource {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
-};
-materialSource material = materialSource(
-    vec3(0.0, 0.0, 0.15),
-    vec3(0.0, 1.0, 0.0),
-    vec3(1.0, 0.0, 0.0),
-    2.5
-);
-
 
 void main() {
     fposition = (model * vec4(position, 1.0)).xyz;
